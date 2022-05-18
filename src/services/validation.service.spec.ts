307a2ace558a2ce917b6f323b4810ae61fff3518
SusanaSalmeron <exdream76@gmail.com>
2022-05-18 12:23:57 +0200

    validationService test

--- src/services/validation.service.spec.ts
+++ src/services/validation.service.spec.ts
@@ -1,20 +1,33 @@
-/* import { Test, TestingModule } from '@nestjs/testing';
-import { DatabaseModule } from '../database/database.module';
-import { ValidationService } from './validation.service'; */
+import { Test, TestingModule } from '@nestjs/testing';
+import { ValidationService } from './validation.service';
+
 
 describe('ValidationService', () => {
-  /* let service: ValidationService;
+  let validationService: ValidationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ValidationService],
-      imports: [DatabaseModule]
     }).compile();
 
-    service = module.get<ValidationService>(ValidationService);
-  }); */
-
-  it('should be defined', () => {
-    /* expect(service).toBeDefined(); */
+    validationService = module.get<ValidationService>(ValidationService);
   });
-});
+
+  it('should be false when pass a non valid email', async () => {
+    const result = await validationService.validateEmail('exdream@gma')
+    expect(result).toBeFalsy()
+  });
+  it('should be true when pass a valid email', async () => {
+    const result = await validationService.validateEmail('exdream@gmail.com')
+    expect(result).toBeTruthy()
+  });
+  it('should be false when pass a non valid password', async () => {
+    const result = await validationService.validatePassword('123')
+    expect(result).toBeFalsy()
+  });
+  it('should be false when pass a non valid password', async () => {
+    const result = await validationService.validatePassword('12345ABc!')
+    expect(result).toBeTruthy()
+  });
+
+})
