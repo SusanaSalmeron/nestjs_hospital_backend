35604555147b9f7d255d5a5f74c1e4d633c8e66f
SusanaSalmeron <exdream76@gmail.com>
2022-05-19 11:23:50 +0200

    authorize doctor middleware test

--- src/middlewares/authorize-doctor.middleware.spec.ts
+++ src/middlewares/authorize-doctor.middleware.spec.ts
@@ -1,7 +1,25 @@
 import { AuthorizeDoctorMiddleware } from './authorize-doctor.middleware';
+import { createMock } from '@golevelup/ts-jest'
 
 describe('AuthorizeDoctorMiddleware', () => {
-  it('should be defined', () => {
-    expect(new AuthorizeDoctorMiddleware()).toBeDefined();
+  const middleware: AuthorizeDoctorMiddleware = new AuthorizeDoctorMiddleware
+  it('should not authorized a doctor ', async () => {
+    const mockRequest = createMock<Request>({
+    })
+
+    try {
+      await middleware.use(mockRequest, null, () => { true })
+      fail("Failed because there is no role")
+    } catch (err) {
+      expect(err.message).toBe('Unauthorized User')
+    }
+  });
+  it('should authorized a doctor ', async () => {
+    const mockRequest = {
+      role: "sanitario"
+    }
+    const mockNext = jest.fn()
+    await middleware.use(mockRequest, null, mockNext)
+    expect(mockNext).toHaveBeenCalled()
   });
 });
