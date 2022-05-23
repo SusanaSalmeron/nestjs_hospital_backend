93e017739d31859234e8cb81bbe4fcbefa8c878e
SusanaSalmeron <exdream76@gmail.com>
2022-05-23 13:01:27 +0200

    patientService test not working properly - TODO

--- src/middlewares/validate-date.middleware.spec.ts
+++ src/middlewares/validate-date.middleware.spec.ts
@@ -1,7 +1,30 @@
 import { ValidateDateMiddleware } from './validate-date.middleware';
 
 describe('ValidateDateMiddleware', () => {
-  it('should be defined', () => {
-    expect(new ValidateDateMiddleware()).toBeDefined();
+  const middleware = new ValidateDateMiddleware
+  it('should throw Invalid date when the date is less than actual date', async () => {
+    const mockRequest = {
+      body: {
+        pickedDate: "04/05/2022"
+      }
+    }
+    const mockNext = jest.fn()
+    try {
+      await middleware.use(mockRequest, null, mockNext)
+    } catch (err) {
+      expect(err.message).toBe('Invalid Date')
+    }
   });
+  it('should validate a date when is greater than actual date', async () => {
+    const mockRequest = {
+      body: {
+        pickedDate: "04/10/2030"
+      }
+    }
+    const mockNext = jest.fn()
+    await middleware.use(mockRequest, null, mockNext)
+    expect(mockNext).toHaveBeenCalled()
+  })
+
+
 });
